3f106cba1e32be374715dc0830c918df217a3b11
Francisco Braço-Forte <f.forte@campus.fct.unl.pt>
2023-10-05 21:02:58 +0100

    enhance benchmark and add csv files for each prog

--- benchmark.sh
+++ benchmark.sh
@@ -1,26 +1,31 @@
 #!/bin/sh
 
-# User defined.
 SZ=512
 RUNS=3
-PROG="sum"
+PROGS="sum sum-omp sum-cuda"
 
-FILE="$PROG.csv"
+make clean
 
-rm -f $FILE
+for p in $PROGS; do
+	echo "running $p..."
 
-echo "optimization,runtime,array_size,init_wall,init_cpu,ref_wall,ref_cpu" >>$FILE
+	file="$p.csv"
 
-for optimization in {0..3}; do
-	for runtime in $(seq 1 $RUNS); do
-		out=$(make -e OPT=$optimization clean $PROG && ./$PROG $SZ)
-		[[ "$?" -ne 0 ]] && exit 1
-		parsed=$(echo $out | awk -F 'gcc' '{print $2}')
-		array_size=$(echo $parsed | cut -d " " -f 12)
-		init_wall=$(echo $parsed | cut -d " " -f 15 | cut -d "=" -f 2)
-		init_cpu=$(echo $parsed | cut -d " " -f 17 | cut -d "=" -f 2)
-		ref_wall=$(echo $parsed | cut -d " " -f 23 | cut -d "=" -f 2)
-		ref_cpu=$(echo $parsed | cut -d " " -f 25 | cut -d "=" -f 2)
-		echo "$optimization,$runtime,$array_size,$init_wall,$init_cpu,$ref_wall,$ref_cpu" >>$FILE
+	rm -f $file
+
+	echo "optimization,runtime,array_size,init_wall,init_cpu,ref_wall,ref_cpu" >>$file
+
+	for optimization in {0..3}; do
+		for runtime in $(seq 1 $RUNS); do
+			out=$(make -e OPT=$optimization $p && ./$p $SZ)
+			[[ "$?" -ne 0 ]] && exit 1
+			parsed=$(echo $out | awk -F 'gcc' '{print $2}')
+			array_size=$(echo $parsed | cut -d " " -f 12)
+			init_wall=$(echo $parsed | cut -d " " -f 15 | cut -d "=" -f 2)
+			init_cpu=$(echo $parsed | cut -d " " -f 17 | cut -d "=" -f 2)
+			ref_wall=$(echo $parsed | cut -d " " -f 23 | cut -d "=" -f 2)
+			ref_cpu=$(echo $parsed | cut -d " " -f 25 | cut -d "=" -f 2)
+			echo "$optimization,$runtime,$array_size,$init_wall,$init_cpu,$ref_wall,$ref_cpu" >>$file
+		done
 	done
 done
